60eb516cc1357a01cdd28dca7c089de219392199
Dariyn <darrenchoy01@gmail.com>
2025-02-25 23:38:45 -0500

    Added mousemove event for interactivity

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 document.addEventListener("mousemove", (event) => {
     let cursor = document.querySelector(".cursor-circle");
-    cursor.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
+
+    // Get mouse position
+    let mouseX = event.clientX;
+    let mouseY = event.clientY;
+
+    // Move the cursor smoothly
+    cursor.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
 });
